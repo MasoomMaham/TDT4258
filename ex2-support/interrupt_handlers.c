96be1d3f189dd2ad0b80ba7bf96d1010e412786d
andigernes <andigernes@users.noreply.github.com>
2015-10-13 12:55:06 +0200

    Update interrupt_handlers.c

--- ex2-support/interrupt_handlers.c
+++ ex2-support/interrupt_handlers.c
@@ -3,10 +3,14 @@
 
 #include "efm32gg.h"
 
+int playMusic();
+void interrupthandler();
+
 /* TIMER1 interrupt handler */
 void __attribute__ ((interrupt)) TIMER1_IRQHandler() 
 {  
-
+	*DAC0_CH0DATA = playMusic();
+  	*DAC0_CH1DATA =	playMusic();
 	*TIMER1_IFC = 1;
   /*
     TODO feed new samples to the DAC
@@ -19,11 +23,59 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 /* GPIO even pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() 
 {
+
+	interrupthandler();
+	*GPIO_IFC = 1;
     /* TODO handle button pressed event, remember to clear pending interrupt */
+	
+	
+	
+	
+	/* else if(button_pressed == 0x40)
+	{
+		int timesPressed = 1;
+		for(lightNumber = 0, timesPressed < 2, lightNumber++)
+		{
+			if(lightNumber == 0)
+			{
+				*GPIO_PA_DOUT = 0x0000001;
+			}
+		}
+	}*/	
 }
 
 /* GPIO odd pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() 
 {
+	interrupthandler();
+	*GPIO_IFC = 1;
     /* TODO handle button pressed event, remember to clear pending interrupt */
+}
+
+void interrupthandler(){
+	uint8_t button_pressed = ~*GPIO_PC_DIN;
+	if(button_pressed == 0x10)
+	{
+		*GPIO_PA_DOUT = 0xFFFFFFFF;
+		//*GPIO_PC_DOUT = 0xFFFFFFFF;
+	}
+	
+	else if(button_pressed == 0x20)
+	{
+		*GPIO_PA_DOUT = 0x0000000;
+		
+	}
+	
+	else if(button_pressed == 0x40)
+	{
+		*GPIO_PA_DOUT = 0xFFFFFFFF;
+		
+	}
+	
+	else if(button_pressed == 0x80)
+	{
+		*GPIO_PA_DOUT = 0xFFFFFFFF;
+		
+	}
+
 }
