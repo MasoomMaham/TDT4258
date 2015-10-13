d6ef0dad8f2ab23854b743e9e0f4a82a43558fb3
andigernes <andigernes@users.noreply.github.com>
2015-10-13 12:53:58 +0200

    Update synthesizer.c

--- ex2-support/synthesizer.c
+++ ex2-support/synthesizer.c
@@ -1,36 +1,51 @@
 #include <stdint.h>
 #include <stdbool.h>
-#include <math.h>
 
 #include "efm32gg.h"
 
-double _radPerCircle = PI*2;
-double _frequency = 2000;
-double _sampleRate = 44100;
+double _radPerCircle = 3.14159265359*2;
+int _frequency = 2000;
+int _sampleRate = 44100;
 
-void setSineOscillator(double sampleRate)
+int calculateSine(double radian);
+
+void setSineOscillator(int sampleRate)
 {
 	_sampleRate = sampleRate;
 }
 
-void setFrequency(double _valueFrequency)
+void setFrequency(int _valueFrequency)
 {
 	_frequency = _valueFrequency;
 }
 
-double getNext(int sampleNumberInSeconds)
+double powerOfInt(double base, int degree)
 {
-	double samplePerOccilation = (_sampleRate/_frequency);
-	double depthIntoOccilations = fmod(sampleNumberInSeconds, samplePerOccilation) / samplePerOccilation;
-	return Math.Sin(depthIntoOccilations * radPerCircle);
+	double powerOfBase = base;
+	for(int powerTimes = 0; powerTimes < degree; powerTimes++)
+	{
+		powerOfBase *= base;
+	}
+	
+	return powerOfBase;
 }
 
-void playMusic()
+int getNext(int sampleNumberInSeconds)
 {
-  for(int i=0; i < sampleRate*2; i++)
-  {
-  	double _musicTone = getNext(i);
-  	*DAC0_CH0DATA = _musicTone;
-  	*DAC0_CH1DATA = _musicTone;
-  }
+	*GPIO_PA_DOUT = 0x0000000;
+	int samplePerOccilation = (_sampleRate/_frequency);
+	double depthIntoOccilations = (samplePerOccilation % sampleNumberInSeconds) / samplePerOccilation;
+	return (int)calculateSine(depthIntoOccilations * _radPerCircle);
+}
+
+int calculateSine(double radian)
+{
+	double sineApprox = radian - (powerOfInt(radian,3)/(3*2*1)) + (powerOfInt(radian,5)/(5*4*3*2*1)) - (powerOfInt(radian,7)/(7*6*5*4*3*2*1));
+	return (int)sineApprox;
+}
+
+int playMusic()
+{	
+	*GPIO_PA_DOUT = 0x0000000;
+	return getNext(3) * 30000;
 }
