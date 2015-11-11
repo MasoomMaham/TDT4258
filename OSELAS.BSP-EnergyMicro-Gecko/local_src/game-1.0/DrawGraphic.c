cc383379abd52d4322a1d7e3a7a0588c35d60d63
andigernes <andigernes@users.noreply.github.com>
2015-11-11 23:20:10 +0100

    Update DrawGraphic.c

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/DrawGraphic.c
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/DrawGraphic.c
@@ -1,7 +1,5 @@
 #include "DrawGraphic.h"
 
-typedef unsigned int uint16_t;
-
 int openFrameBuffer;
 int pixelsOnScreen;
 int totalBytesUsedByScreen;
@@ -20,10 +18,10 @@ bool ballStickedToPlayer = false;
 
 int framebuffer()
 {
-	openFrameBuffer = open("/dev/fb0", O_RDWR);
+	openFrameBuffer = (int)fopen("/dev/fb0", O_RDWR);
 	if(!openFrameBuffer)
 	{
-		printk(KERN_INFO "Could not open the file fb0 or use the framebuffer");
+		printf("Could not open the file fb0 or use the framebuffer\n");
 		return EXIT_FAILURE;
 	}
 	
@@ -35,28 +33,29 @@ int memoryMapDriver()
 	framebuffer();
 	if(ioctl(openFrameBuffer, FBIOGET_VSCREENINFO, &screenInfo) == -1)
 	{
-		printk(KERN_INFO "ERROR: Could not retrive the screen info\n");
-		return EXIT_FAILURE:
-	}
-
-	printf("Screeninfo: %d x %d, %dbpp\n", screenInfo.xres, screenInfo.yres, screenInfo.bits_per_pixel);
-	
-	screen.dx = 0;
-	screen.dy = 0;
-	screen.width = screenInfo.xres;
-	screen.height = screenInfo.yres;
-
-	pixelsOnScreen = screen.width * screen.height;
-	totalBytesUsedByScreen = pixelsOnScreen * screenInfo.bits_per_pixel / 8;	
-
-	uint16_t *screen = (uint16_t)mmap(NULL, totalBytesUsedByScreen, PROT_READ|PROT_WRITE, MAP_SHARED, openFrameBuffer, 0);
-	if((int)screen == MAP_FAILED)
-	{
-		printk(KERN_INFO "Memorymap the framebuffer most likely failed\n");
+		printf("ERROR: Could not retrive the screen info\n");
 		return EXIT_FAILURE;
 	}
 
-	for (int totalPixels = 0; totalPixels < pixelsOnScreen; totalPixels++)
+	//printf("Screeninfo: %d x %d, %dbpp\n", screenInfo.xres, screenInfo.yres, screenInfo.bits_per_pixel);
+	
+	screenArea.dx = 0;
+	screenArea.dy = 0;
+	screenArea.width = screenInfo.xres;
+	screenArea.height = screenInfo.yres;
+
+	pixelsOnScreen = screenArea.width * screenArea.height;
+	totalBytesUsedByScreen = pixelsOnScreen * screenInfo.bits_per_pixel / 8;	
+
+	(uintptr_t) screen = mmap(NULL, totalBytesUsedByScreen, PROT_READ|PROT_WRITE, MAP_SHARED, openFrameBuffer, 0);
+	if((uintptr_t)screen == (uintptr_t)MAP_FAILED)
+	{
+		printf("Memorymap the framebuffer most likely failed\n");
+		return EXIT_FAILURE;
+	}
+	
+	int totalPixels;
+	for (totalPixels = 0; totalPixels < pixelsOnScreen; totalPixels++)
 	{ 
 		screen[totalPixels] = BACKGROUND_COLOR1;
 	}
@@ -66,7 +65,7 @@ int memoryMapDriver()
 
 void updateBufferDriver()
 {
-	ioctl(openFrameBuffer, 0x4680, &screenArea)
+	ioctl(openFrameBuffer, 0x4680, &screenArea);
 }
 
 void disconnect_frameBuffer()
@@ -75,18 +74,20 @@ void disconnect_frameBuffer()
 	close(openFrameBuffer); 
 }
 
-void draw_game();
+void draw_game()
 {
-	for(int row = 0; row < screen_height; row++)
+	int row;
+	int col;
+	for(row = 0; row < screen_height; row++)
 	{
-		if(row < screenHeight_margin || row > (screen_sectors_y_axis + screenHeight_margin) && row < (screen_height - (PLAYER_HEIGHT + (BALL_RADIUS*2))))
+		if(row < screenHeight_margin || (row > (screen_sectors_y_axis + screenHeight_margin) && row < (screen_height - (PLAYER_HEIGHT + (BALL_RADIUS*2)))))
 		{
 			continue;
 		}
 		
-		for(int col = 0; col < screen_width; col++)
+		for(col = 0; col < screen_width; col++)
 		{
-			if(row > screenHeight_margin && col > (screenWidth_margin/2) && row < (screen_sectors_y_axis + screenHeight_margin) && col < (screen_width - screenWidth_margin/2) 
+			if(row > screenHeight_margin && col > (screenWidth_margin/2) && row < (screen_sectors_y_axis + screenHeight_margin) && col < (screen_width - screenWidth_margin/2)) 
 			{
 				draw_Brick(col, row, BRICK_WIDTH, BRICK_HEIGHT);
 				col += BRICK_WIDTH + brickWidth_margin;
@@ -96,17 +97,17 @@ void draw_game();
 				} 		
 			}
 			
-			if(row > (screen_height - (screenBottom_margin + PLAYER_HEIGHT + BALL_RADIUS)) && col > (screen_width/2) && col < (screen_width/2) && row < (screen_height - (screenBottom_margin + PLAYER_HEIGHT + BALL_RADIUS))) 
+			else if(row > (screen_height - (screenBottom_margin + PLAYER_HEIGHT + BALL_RADIUS)) && col > (screen_width/2) && col < (screen_width/2) && row < (screen_height - (screenBottom_margin + PLAYER_HEIGHT + BALL_RADIUS))) 
 			{
 				row = screen_height - (PLAYER_HEIGHT + (BALL_RADIUS));
-				col = (screen_width/2)
-				draw_Ball(row, col, radius);	
+				col = (screen_width/2);
+				draw_Ball(row, col, BALL_RADIUS);	
 			}
 			
-			if(row > (screen_height - (screenBottom_margin + PLAYER_HEIGHT)) && col > ((screen_width/2) - (PLAYER_WIDTH/2)) && col < ((screen_width/2) + (PLAYER_WIDTH/2))) && row < (screen_height - screenBottom_margin))
+			else if(row > (screen_height - (screenBottom_margin + PLAYER_HEIGHT)) && col > ((screen_width/2) - (PLAYER_WIDTH/2)) && col < ((screen_width/2) + (PLAYER_WIDTH/2)) && row < (screen_height - screenBottom_margin))
 			{
-				startPos_x = ((screen_width/2) - (PLAYER_WIDTH/2))
-				startPos_y = screen_height - (screenBottom_margin + PLAYER_HEIGHT)
+				int startPos_x = ((screen_width/2) - (PLAYER_WIDTH/2));
+				int startPos_y = screen_height - (screenBottom_margin + PLAYER_HEIGHT);
 				draw_Player(startPos_x , startPos_y, PLAYER_WIDTH, PLAYER_HEIGHT);
 				row += PLAYER_HEIGHT;
 				col += PLAYER_WIDTH; 
@@ -115,24 +116,10 @@ void draw_game();
 	}
 }
 
-void draw_Brick(int pen_position_x, int pen_position_y, int width, int height);
-{
-	int movePenDistance = 0;
-	int penDrawLength_x = pen_position_x + width;
-	int penDrawLength_y = pen_position_y + height;
-	
-	for(movePenDistance = pen_position_y; movePenDistance < penDrawLength_y; movePenDistance++)
-	{
-		draw_line(pen_position_x, movePenDistance, penDrawLength_x, movePenDistance);
-	}	
-}
-
-void draw_line(int startPosition_x, startPosition_y, endPosition_x, endPosition_y)
+void draw_line(int startPosition_x, int startPosition_y, int endPosition_x, int endPosition_y)
 {
 	int randomColor = rand() % 6;
 	uint16_t fillColor = colors[randomColor]; 
-	int x_distance = abs(endPosition_x - startPosition_x);
-	int y_distance = -abs(endPosition_y - startPosition_y);
 	bool finish = false;
 	while(!finish)
 	{
@@ -149,6 +136,18 @@ void draw_line(int startPosition_x, startPosition_y, endPosition_x, endPosition_
 	}
 }
 
+void draw_Brick(int pen_position_x, int pen_position_y, int width, int height)
+{
+	int movePenDistance = 0;
+	int penDrawLength_x = pen_position_x + width;
+	int penDrawLength_y = pen_position_y + height;
+	
+	for(movePenDistance = pen_position_y; movePenDistance < penDrawLength_y; movePenDistance++)
+	{
+		draw_line(pen_position_x, movePenDistance, penDrawLength_x, movePenDistance);
+	}	
+}
+
 void fillPixel(int startPosition_x , int startPosition_y, uint16_t fillColor)
 {
 	screen[startPosition_y * screen_width + startPosition_x] = fillColor; 
@@ -156,10 +155,12 @@ void fillPixel(int startPosition_x , int startPosition_y, uint16_t fillColor)
 
 void draw_Ball(int row, int col, int radius)
 {
+	int dy;
+	int dx;
 	int diameter = radius * radius;
-	for(int dy = -radius; dy <= radius; dy++)
+	for(dy = -radius; dy <= radius; dy++)
 	{
-		for(int dx = -radius; dx <= radius; dx++)
+		for(dx = -radius; dx <= radius; dx++)
 		{
 			if((dx*dx + dy*dy) <= diameter)
 			{
@@ -170,7 +171,7 @@ void draw_Ball(int row, int col, int radius)
 }
 
 
-void draw_Player(int pen_position_x, int pen_position_y, int width, int height);
+void draw_Player(int pen_position_x, int pen_position_y, int width, int height)
 {	
 	int movePenDistance = 0;
 	int penDrawLength_x = pen_position_x + width;
@@ -185,25 +186,29 @@ void draw_Player(int pen_position_x, int pen_position_y, int width, int height);
 void draw_movedPlayer(int pen_position_x)
 {
 	int movePenDistance = 0;
+	int penDrawLength_x = 0;
 	if(pen_position_x >= 0)
 	{
-		int penDrawLength_x = playerWidthStart + pen_position_x + (PLAYER_WIDTH/2);
+		penDrawLength_x = playerWidthStart + pen_position_x;
 	}
 	
 	else if(pen_position_x < 0)
 	{
-		int penDrawLength_x = playerWidthStart - pen_position_x - (PLAYER_WIDTH/2);
+		penDrawLength_x = playerWidthStart - pen_position_x;
 	}
 	
-	int penDrawLength_y = screenHeight - screenBottom_margin - PlayerHeight;
-	for(movePenDistance = pen_position_y; movePenDistance < penDrawLength_y; movePenDistance++)
+	int penDrawLength_y = screen_height - screenBottom_margin - PLAYER_HEIGHT;
+	for(movePenDistance = penDrawLength_y; movePenDistance < (penDrawLength_y + PLAYER_HEIGHT); movePenDistance++)
 	{
 		draw_line(pen_position_x, movePenDistance, penDrawLength_x, movePenDistance);	
 }
 
 void draw_movedBall(int row, int col, bool ballReleased)
 {	
-	
+
+	int dy;
+	int dx;
+	int diameter = BALL_RADIUS * BALL_RADIUS;	
 	if(col < 0)
 	{
 		ballCenter -= col;
@@ -216,10 +221,9 @@ void draw_movedBall(int row, int col, bool ballReleased)
 
 	if(!ballReleased)
 	{
-		int diameter = BALL_RADIUS * BALL_RADIUS;
-		for(int dy = -BALL_RADIUS; dy <= BALL_RADIUS; dy++)
+		for(dy = -BALL_RADIUS; dy <= BALL_RADIUS; dy++)
 		{
-			for(int dx = -BALL_RADIUS; dx <= BALL_RADIUS; dx++)
+			for(dx = -BALL_RADIUS; dx <= BALL_RADIUS; dx++)
 			{
 				if((dx*dx + dy*dy) <= diameter)
 				{
@@ -233,14 +237,13 @@ void draw_movedBall(int row, int col, bool ballReleased)
 	{
 		ballStickedToPlayer = ballReleased;
 		ballRowLocation -= row;
-		int diameter = BALL_RADIUS * BALL_RADIUS;
-		for(int dy = -BALL_RADIUS; dy <= BALL_RADIUS; dy++)
+		for(dy = -BALL_RADIUS; dy <= BALL_RADIUS; dy++)
 		{
-			for(int dx = -BALL_RADIUS; dx <= BALL_RADIUS; dx++)
+			for(dx = -BALL_RADIUS; dx <= BALL_RADIUS; dx++)
 			{
 				if((dx*dx + dy*dy) <= diameter)
 				{
-					fillPixel((ballRowLocation + diameter, ballCenter + diameter, White); 
+					fillPixel(ballRowLocation + diameter, ballCenter + diameter, White); 
 				}
 			}
 		} 
@@ -251,17 +254,19 @@ void ballMovementAfterRelease(int row, int col)
 {
 	ballRowLocation -= row;
 	ballCenter += col;
+	int dy;
+	int dx;
 	int diameter = BALL_RADIUS * BALL_RADIUS;
-	for(int dy = -BALL_RADIUS; dy <= BALL_RADIUS; dy++)
+	for(dy = -BALL_RADIUS; dy <= BALL_RADIUS; dy++)
+	{
+		for(dx = -BALL_RADIUS; dx <= BALL_RADIUS; dx++)
 		{
-			for(int dx = -BALL_RADIUS; dx <= BALL_RADIUS; dx++)
+			if((dx*dx + dy*dy) <= diameter)
 			{
-				if((dx*dx + dy*dy) <= diameter)
-				{
-					fillPixel((ballRowLocation + diameter, ballCenter + diameter, White); 
-				}
+				fillPixel(ballRowLocation + diameter, ballCenter + diameter, White);
 			}
-		} 
-	 
+		}
+	} 	 
+}
 }
 
