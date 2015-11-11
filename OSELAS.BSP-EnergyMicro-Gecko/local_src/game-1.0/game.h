436d4be97027cea147623b888b0cfe2a2a548179
andigernes <andigernes@users.noreply.github.com>
2015-11-11 13:18:26 +0100

    Create game.h

---
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "DrawGraphics.h"
+
+//Prototypes:
+int initialize();
+void deinitialize();
+int initialize_gamepad();
+int initialize_framebuffer();
+void tearDown_gamepad();
+void signal_handler(int signaloutput);
+int signal(void SIGIO,int &signal_handler);
+int main();
